5215b9ec312f5f53ad80330dbb93ac1865d0c602
unknown <teodor.popa96@gmail.com>
2023-09-19 20:23:52 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,239 @@
+/*
+console.log("Hello World!");
+*/
+
+/*
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+  {
+    text: "Jonas",
+  },
+];
+
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+// factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+
+/* const htmlArr = initialFacts.map(
+  (fact) => `<li class="fact"<${fact.text}</li>`
+);
+console.log(htmlArr);
+const html = htmlArr.join("");
+factsList.insertAdjacentHTML("afterbegin", html);
+
+// Toggle form visibility
+btn.addEventListener("click", function () {
+  factsList.innerHTML = "";
+
+  console.log("CLICK");
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+/*
+let votesInteresting = 23;
+let votesMindBlowing = 5;
+const text = "Lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindBlowing;
+console.log("Upvotes", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
+
+console.log(parseInt("24.533ccc"));
+
+function calcFactAge(year) {
+  const currentYear = 2023;
+  // 2023-2015
+  const age = currentYear - year;
+  return age;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+*/
+
+/*
+let votesInteresting = 20;
+let votesMindBlowing = 0;
+if (votesInteresting === votesMindBlowing) {
+  alert("This fact is equallu interesting and mindblowing");
+} else if (votesInteresting > votesMindBlowing) {
+  console.log("Interesting fact!");
+} else if (votesInteresting < votesMindBlowing) {
+  console.log("Mindblowing fact!!");
+}
+
+// falsy values: 0, '', null, undefined
+// truthy value: everything else..
+if (votesMindBlowing) {
+  console.log("MINDBLOWING FACT!!");
+} else {
+  console.log("Not so special..");
+}
+
+let votesFalse = 7;
+const totalUpvotes = votesInteresting + votesMindBlowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "Might be false,check the sources...";
+
+//alert(message);
+
+const text = "Lisbon is the capital of Portugal";
+const upperText = text.toUpperCase();
+console.log(upperText);
+
+const str = `The current fact is "${text}"`;
+console.log(str);
+*/
+
+/*
+const fact = ["Lisbon is the capital of Portugal", 2015, true, "something"];
+console.log(fact);
+console.log(fact[0]);
+console.log(fact.length);
+console.log(fact[fact.length - 1]);
+
+const [text, createdIn, isCorrect] = fact;
+console.log(text);
+
+const newFact = [fact, "society"];
+console.log(newFact);
+*/
+
+/*
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact ${
+      this.text
+    } "is from the category ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj["text"]);
+console.log(factObj.createSummary());
+*/
+
+/*looping
+[2, 4, 6, 8].forEach(function (el) {
+  console.log(el);
+});
+*/
+
+/*
+const times10 = [2, 4, 6, 8].map((el) => el * 10);
+console.log(times10);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function calcFactAge(year) {
+  const currentYear = 2023;
+  // 2023-2015
+  const age = currentYear - year;
+  return age;
+}
+
+const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+console.log(factAges);
+*/
